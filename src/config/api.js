19213d2df7c62fb92f578d427772e5adf1b575aa
Admin <ishwarparmar84@gmail.com>
2026-06-10 23:20:30 +0530

    added netilify

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,5 @@
-export const API_BASE_URL = 'http://127.0.0.1:8080/';
+// export const API_BASE_URL = 'http://127.0.0.1:8080/';
 // python
 //export const API_BASE_URL = 'http://127.0.0.1:5000/';
+
+export const API_BASE_URL = 'https://doctorbaddy-backend.onrender.com/';
